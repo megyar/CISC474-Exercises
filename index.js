888df50d52d7422068e7f6205627f389383204b1
Meghana Yarlagadda <megyar@udel.edu>
2023-09-07 13:15:16 -0400

    Added click event to display name

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    $("#fname").click(function(){
+        $(this).val("");
+    });
+
+    $("#lname").click(function(){
+        $(this).val("");
+    });
+
+    $("#go").on("click", function(event) {
+        console.log("click handler");
+        var name = $("#fname").val() + " " + $("#lname").val();
+        $("#jumbotron").text(name);
+        return false;
+    });
+    return false;
 });
